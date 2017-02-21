12ce90cf7c837f389c05961b3f3d399730e94278
trial <nguyenvanthu20112265@gmail.com>
2017-02-21 18:40:53 +0700

    create bot

--- src/com/java/qzking/Bot.java
+++ src/com/java/qzking/Bot.java
@@ -29,11 +29,11 @@ public class Bot implements IEventListener {
 	private String botName;
 	private List<Integer> userIds = new ArrayList<>();
 	private int counter = 0;
-	private ScheduledExecutorService timer = Executors.newScheduledThreadPool(2);
+	private ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
 
-	public Bot(BotType type, String name) {
+	public Bot(BotType type, int index) {
 		this.setType(type);
-		this.botName = name;
+		this.botName = "qzkingBot " + index;
 		System.out.println("start bot with type: " + type);
 		init();
 	}
@@ -45,8 +45,8 @@ public class Bot implements IEventListener {
 	public void start() {
 		connect();
 	}
-	
-	public void stop(){
+
+	public void stop() {
 		disconnect();
 	}
 
@@ -60,7 +60,7 @@ public class Bot implements IEventListener {
 	}
 
 	private void connect() {
-		System.out.println("connecting to server .....");
+		System.out.println(this.botName + " connecting to server .....");
 		this.smartFox.connect(ServerConfig.SERVER_ADDRESS, ServerConfig.PORT);
 	}
 
@@ -69,156 +69,19 @@ public class Bot implements IEventListener {
 		this.timer.shutdown();
 	}
 
-	private void onConnection(BaseEvent event) {
-		System.out.println("connection to server success");
-		login();
-	}
-
-	private void onConnectionLost(BaseEvent event) {
-		System.out.println("connection fail, try again");
-		counter += 1;
-		if (counter > 5) {
-			disconnect();
-		} else {
-			start();
-		}
-	}
-
-	private void onLogin(BaseEvent event) {
-		// on login response
-		System.out.println("on login response ");
-	}
-
-	private void onJoinRoom(BaseEvent event) {
-		System.out.println(this.botName + " join room");
-		if (this.userIds.size() > 0) {
-			for (int userId : this.userIds) {
-				sendInviteRequest(userId);
-			}
-		}
-		monitorLag();
-	}
-
-	private void onExtensionResponse(BaseEvent event) {
-		String command = (String) event.getArguments().get("cmd");
-		System.out.println("receive " + command);
-		ISFSObject params = (SFSObject) event.getArguments().get("params");
-		ISFSObject obj = new SFSObject();
-		switch (command) {
-		// ONLINE LIST
-		case "27":
-			if (this.getType() == BotType.AUTO_INVITE) {
-				SFSArray arr = new SFSArray();
-				arr = (SFSArray) params.getSFSArray("lFound");
-				if (arr != null) {
-					System.out.println("number user online is: " + arr.size());
-					for (int i = 0; i < arr.size(); i++) {
-						SFSObject userOnline = (SFSObject) arr.getSFSObject(i);
-						int userId = userOnline.getInt("id");
-						String displayName = userOnline.getUtfString("display");
-						System.out.println("user " + userId + " with " + displayName);
-						this.userIds.add(userId);
-					}
-				} else {
-					System.out.println("no body in server ");
-					startGameSolo();
-				}
-			}
-			break;
-		// CLIENT_INVITE
-		case "30":
-			String roomName = params.getUtfString("rName");
-			String ownerName = params.getUtfString("ownerName");
-			String ownerID = params.getUtfString("ownerID");
-			if (roomName == null) {
-				break;
-			}
-			System.out.println(roomName + " & " + ownerName + " & " + ownerID);
-			obj.putUtfString("rName", roomName);
-			System.out.println(this.botName + " send accept room" + roomName);
-			obj.putBool("accept", true);
-			smartFox.send(new ExtensionRequest(Event.CLIENT_INVITERESPONSE, obj));
-			obj = null;
-			break;
-		// CLIENT_ANSWER
-		case "12":
-			System.out.println(this.botName + " send answer");
-			int currentQuiz = params.getInt("currQuiz");
-			String question = params.getUtfString("question");
-			String correctAnswer = params.getUtfString("correctAnswer");
-			System.out.println(this.botName + " play quiz " + currentQuiz + " question: " + question);
-			Random random = new Random();
-			int i = random.nextInt(3 - 0 + 1) + 0;
-			ArrayList<String> s = new ArrayList<String>(params.getUtfStringArray("answer"));
-			String randomAns = s.get(i);
-			System.out.println("random answer: " + randomAns);
-			obj = new SFSObject();
-			obj.putUtfString("answer", correctAnswer);
-			smartFox.send(new ExtensionRequest(Event.ANSWER, obj));
-			break;
-		// LIST_QUIZ
-		case "12.5":
-			ISFSArray quizArr = new SFSArray();
-			quizArr = (SFSArray) params.getSFSArray("lQ");
-			System.out.println("number quizs: " + quizArr.size());
-			break;
-		// CLIENT_START
-		case "9":
-			int currQuiz = params.getInt("currQuiz");
-			int time = params.getInt("time");
-			int type = params.getInt("solo");
-			System.out.println(currQuiz + " & " + time + " & " + type);
-			break;
-		case "11":
-			System.out.println("bot's game stopped");
-			break;
-		// ERROR
-		case "0":
-			if (params.containsKey("msg")) {
-				String error = params.getUtfString("msg");
-				System.out.println("has error: " + error);
-			}
-			if (params.containsKey("code")) {
-				int errorCode = params.getInt("code");
-				System.out.println("error code: " + errorCode);
-			}
-			restart();
-			break;
-		default:
-			break;
-		}
-	}
-
-	@Override
-	public void dispatch(BaseEvent event) throws SFSException {
-		String type = event.getType();
-		if (type.equalsIgnoreCase(SFSEvent.CONNECTION)) {
-			if (event.getArguments().get("success").equals(true)) {
-				onConnection(event);
-			} else {
-				onConnectionLost(event);
-			}
-		} else if (type.equalsIgnoreCase(SFSEvent.CONNECTION_LOST)) {
-			onConnectionLost(event);
-		} else if (type.equalsIgnoreCase(SFSEvent.LOGIN)) {
-			onLogin(event);
-		} else if (type.equalsIgnoreCase(SFSEvent.ROOM_JOIN)) {
-			onJoinRoom(event);
-		} else if (type.equalsIgnoreCase(SFSEvent.EXTENSION_RESPONSE)) {
-			onExtensionResponse(event);
-		}
-	}
-
 	private void login() {
 		ISFSObject obj = new SFSObject();
-		obj.putUtfString("did", "test");
+		obj.putUtfString("did", "qzkingBot");
+		obj.putUtfString("displayName", this.botName);
 		// obj.putUtfString("fid", "test");
 		// obj.putUtfString("em", "test");
 
 		LoginRequest request = new LoginRequest(this.botName, this.botName, ServerConfig.ZONE, obj);
 		this.smartFox.send(request);
 	}
-	private void restart(){
+
+	@SuppressWarnings("unused")
+	private void restart() {
 		stop();
 		start();
 	}
@@ -258,6 +121,158 @@ public class Bot implements IEventListener {
 		ISFSObject params = new SFSObject();
 		params.putBool("solo", true);
 		this.smartFox.send(new ExtensionRequest("9", params));
+	}
+
+	private void onConnection(BaseEvent event) {
+		System.out.println(this.botName + " connection to server success");
+		login();
+	}
+
+	private void onConnectionLost(BaseEvent event) {
+		System.out.println(this.botName + " connection fail, try again");
+		counter += 1;
+		if (counter > 5) {
+			disconnect();
+		} else {
+			start();
+		}
+	}
+
+	private void onLogin(BaseEvent event) {
+		// on login response
+		System.out.println(this.botName + " on login response ");
+	}
+
+	private void onJoinRoom(BaseEvent event) {
+		System.out.println(this.botName + " join room");
+		if (this.getType() == BotType.AUTO_INVITE) {
+			if (this.userIds.size() > 0) {
+				System.out.println("number user onlines is: " + this.userIds.size());
+				for (int userId : this.userIds) {
+					sendInviteRequest(userId);
+				}
+			} else {
+//				startGameSolo();
+			}
+		}
+
+		monitorLag();
+	}
+
+	private void onExtensionResponse(BaseEvent event) {
+		String command = (String) event.getArguments().get("cmd");
+		System.out.println(this.botName + " receive command " + command);
+		ISFSObject params = (SFSObject) event.getArguments().get("params");
+		ISFSObject obj = new SFSObject();
+		switch (command) {
+		// ONLINE LIST
+		case "27":
+			SFSArray arr = new SFSArray();
+			arr = (SFSArray) params.getSFSArray("lFound");
+			if (arr != null) {
+				for (int i = 0; i < arr.size(); i++) {
+					SFSObject userOnline = (SFSObject) arr.getSFSObject(i);
+					int userId = userOnline.getInt("id");
+					String displayName = userOnline.getUtfString("display");
+					System.out.println("user " + userId + " with " + displayName);
+					if (!displayName.contains("qzkingBot")){
+						this.userIds.add(userId);
+					}
+				}
+			} else {
+				System.out.println("no body in server ");
+
+			}
+
+			break;
+		// CLIENT_INVITE
+		case "30":
+			String roomName = params.getUtfString("rName");
+			String ownerName = params.getUtfString("ownerName");
+			String ownerID = params.getUtfString("ownerID");
+			if (roomName == null) {
+				break;
+			}
+			System.out.println(roomName + " & " + ownerName + " & " + ownerID);
+			obj.putUtfString("rName", roomName);
+			System.out.println(this.botName + " send accept room" + roomName);
+			obj.putBool("accept", true);
+			smartFox.send(new ExtensionRequest(Event.CLIENT_INVITERESPONSE, obj));
+			obj = null;
+			break;
+		// client receive quiz -> send answer
+		case "12":
+			System.out.println(this.botName + " send answer");
+			int currentQuiz = params.getInt("currQuiz");
+			String question = params.getUtfString("question");
+			String correctAnswer = params.getUtfString("correctAnswer");
+			System.out.println(this.botName + " play quiz " + currentQuiz + " question: " + question);
+			Random random = new Random();
+			int i = random.nextInt(3 - 0 + 1) + 0;
+			System.out.println("random answer: " + i + " && correct answer: " + correctAnswer);
+			obj = new SFSObject();
+			obj.putUtfString("answer", correctAnswer);
+			obj.putInt("awsId", i);
+			Random rand = new Random();
+			int sleep = rand.nextInt(10);
+			try {
+				Thread.sleep(sleep*1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			smartFox.send(new ExtensionRequest(Event.ANSWER, obj));
+			break;
+		// LIST_QUIZ
+		case "12.5":
+			ISFSArray quizArr = new SFSArray();
+			quizArr = (SFSArray) params.getSFSArray("lQ");
+			System.out.println("number quizs: " + quizArr.size());
+			break;
+		// CLIENT_START
+		case "9":
+			int currQuiz = params.getInt("currQuiz");
+			int time = params.getInt("time");
+			int type = params.getInt("solo");
+			System.out.println(currQuiz + " & " + time + " & " + type);
+			break;
+		case "11":
+			System.out.println("bot's game stopped");
+			break;
+		// ERROR
+		case "0":
+			if (params.containsKey("msg")) {
+				String error = params.getUtfString("msg");
+				System.out.println("has msg error: " + error);
+			}
+			if (params.containsKey("code")) {
+				int errorCode = params.getInt("code");
+				System.out.println("error code: " + errorCode);
+			}
+			// restart();
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	public void dispatch(BaseEvent event) throws SFSException {
+		String type = event.getType();
+		if (type.equalsIgnoreCase(SFSEvent.CONNECTION)) {
+			if (event.getArguments().get("success").equals(true)) {
+				onConnection(event);
+			} else {
+				onConnectionLost(event);
+			}
+		} else if (type.equalsIgnoreCase(SFSEvent.CONNECTION_LOST)) {
+			onConnectionLost(event);
+		} else if (type.equalsIgnoreCase(SFSEvent.LOGIN)) {
+			onLogin(event);
+		} else if (type.equalsIgnoreCase(SFSEvent.ROOM_JOIN)) {
+			onJoinRoom(event);
+		} else if (type.equalsIgnoreCase(SFSEvent.EXTENSION_RESPONSE)) {
+			onExtensionResponse(event);
+		}
 	}
 
 	public BotType getType() {
--- src/com/java/qzking/conf/ServerConfig.java
+++ src/com/java/qzking/conf/ServerConfig.java
@@ -1,7 +1,8 @@
 package com.java.qzking.conf;
 
 public class ServerConfig {
-	public static final String SERVER_ADDRESS = "162.13.141.67";
+	//162.13.141.67
+	public static final String SERVER_ADDRESS = "139.162.5.38";
 	public static final int PORT = 843;
 	public final static boolean DEBUG_SFS = false;
 	public static final String ZONE = "KQ1";
